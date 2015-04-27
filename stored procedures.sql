bcd2d672d92cf2eac298a64c85598d6670b1282d
MORourke3 <mjo95jr@aol.com>
2015-04-26 22:45:17 -0400

    Stored procedures lab
    Lines 91 - 128. Both stored procedures.

---
+++ stored procedures.sql
@@ -0,0 +1,128 @@
+﻿----------------------------------------------------------------------------------------
+-- Courses and Prerequisites
+-- by Alan G. Labouseur
+-- Tested on Postgres 9.3.2
+----------------------------------------------------------------------------------------
+
+--
+-- The table of courses.
+--
+create table Courses (
+    num      integer not null,
+    name     text    not null,
+    credits  integer not null,
+  primary key (num)
+);
+
+
+insert into Courses(num, name, credits)
+values (499, 'CS/ITS Capping', 3 );
+
+insert into Courses(num, name, credits)
+values (308, 'Database Systems', 4 );
+
+insert into Courses(num, name, credits)
+values (221, 'Software Development Two', 4 );
+
+insert into Courses(num, name, credits)
+values (220, 'Software Development One', 4 );
+
+insert into Courses(num, name, credits)
+values (120, 'Introduction to Programming', 4);
+
+select * 
+from courses
+order by num ASC;
+
+
+--
+-- Courses and their prerequisites
+--
+create table Prerequisites (
+    courseNum integer not null references Courses(num),
+    preReqNum integer not null references Courses(num),
+  primary key (courseNum, preReqNum)
+);
+
+insert into Prerequisites(courseNum, preReqNum)
+values (499, 308);
+
+insert into Prerequisites(courseNum, preReqNum)
+values (499, 221);
+
+insert into Prerequisites(courseNum, preReqNum)
+values (308, 120);
+
+insert into Prerequisites(courseNum, preReqNum)
+values (221, 220);
+
+insert into Prerequisites(courseNum, preReqNum)
+values (220, 120);
+
+select *
+from Prerequisites
+order by courseNum DESC;
+
+
+--
+-- An example stored procedure ("function")
+--
+create or replace function get_courses_by_credits(int, REFCURSOR) returns refcursor as 
+$$
+declare
+   num_credits int       := $1;
+   resultset   REFCURSOR := $2;
+begin
+   open resultset for 
+      select num, name, credits
+      from   courses
+       where  credits >= num_credits;
+   return resultset;
+end;
+$$ 
+language plpgsql;
+
+select get_courses_by_credits(0, 'results');
+Fetch all from results;
+
+
+
+
+-- Returns immediate prereqs of passed in course
+create or replace function get_prereqs(int, REFCURSOR) returns refcursor as 
+$$
+declare
+   num_course int       := $1;
+   resultset   REFCURSOR := $2;
+begin
+   open resultset for 
+      select courseNum, preReqNum
+      from   Prerequisites;
+
+   return resultset;
+end;
+$$ 
+language plpgsql;
+
+select get_prereqs (0, 'results');
+Fetch all from results;
+
+
+-- Returns courses for which passed in course is a prereq
+create or replace function get_postreq(int, REFCURSOR) returns refcursor as 
+$$
+declare
+   num_course int       := $1;
+   resultset   REFCURSOR := $2;
+begin
+   open resultset for 
+      select preReqNum, courseNum
+      from Prerequisites;
+
+   return resultset;
+end;
+$$ 
+language plpgsql;
+
+select get_postreq (0, 'results');
+Fetch all from results;
